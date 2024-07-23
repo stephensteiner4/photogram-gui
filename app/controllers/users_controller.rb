affed858e2175a1cc7d587501f19fea30178395d
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-23 01:51:20 +0000

    include update user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,21 @@ class UsersController < ApplicationController
 
     render({ :template => "/user_templates/details" })
   end
+
+  def update
+    the_username = params.fetch("user")
+    new_name = params.fetch("edit_username")
+
+    @the_user = User.where({:username => the_username}).at(0)
+
+    @the_user.username = new_name
+
+    if @the_user.valid?
+      @the_user.save
+
+      redirect_to("/users/#{new_name}", {:notice => "Username updated successfully."})
+    else
+      redirect_to("/users/#{the_username}", {:alert => "Username unavailable, please try again."})
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:user", {:controller => "users", :action => "details"})
+  post("/modify_user/:user", {:controller => "users", :action => "update"})
 
   get("/photos", {:controller =>"photo", :action => "index"})
   get("/photos/:path_id", {:controller =>"photo", :action => "details"})
